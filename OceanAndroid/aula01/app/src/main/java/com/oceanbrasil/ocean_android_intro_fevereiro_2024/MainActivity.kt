6af88dbaeaeabaf2ff8ade0f11fcefae5d39642c
AdemarCastro <2022002444@ifam.edu.br>
2024-02-09 11:31:10 -0400

    Curso 'Introdução: Android' finalizado

--- OceanAndroid/aula01/app/src/main/java/com/oceanbrasil/ocean_android_intro_fevereiro_2024/MainActivity.kt
+++ OceanAndroid/aula01/app/src/main/java/com/oceanbrasil/ocean_android_intro_fevereiro_2024/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.oceanbrasil.ocean_android_intro_fevereiro_2024
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -7,38 +8,62 @@ import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-
-    // Criar aplicações que funcionam para todos os dispositivos - Versão mais simples do JDK
-    // Ele avisa que algum recurso da versão mais nova não está disponível na versão mais antiga
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Criar um listener para o botão
-        // Encontrar o botão pelo ID
+        // Precisamos encontrar o botão pelo ID
 
-        // findViewById => método que encontra View no Layout usando o ID
+        // findViewById -> método que encontra View no Layout usando o ID
         // Dentro dos <>, informamos o tipo do elemento que estamos buscando
-        // Dentro dos (), informamos o ID do elemento, usando o R.id para buscar
+        // Dentro dos (), informamos o ID do elemento, usando R.id para buscar
         // Depois, armazenamos isso numa variável, para utilizarmos depois
         // val -> variável que inicia com um valor e esse valor nunca muda
-        // var -> variável que iniciar com um valor e ease valor pode mudar
+        // var -> variável que iniciar com um valor e esse valor pode mudar
         val btEnviar = findViewById<Button>(R.id.btEnviar)
+        val etNome = findViewById<EditText>(R.id.inputText)
         val tvResultado = findViewById<TextView>(R.id.tvResultado)
-        val inpResult = findViewById<EditText>(R.id.inputText)
 
         // Criar um Listener para o botão btEnviar
         btEnviar.setOnClickListener {
             // Dentro das chaves {}, o código será executado
-            // Quando o clique (toque) ocorrer no botão
-            // tvResultado.text = "Ademar Castro!!"
+            // quando o clique (toque) ocorrer no botão
 
-            // Caso o inputResultado seja nulo ou vázio
-            if (inpResult.text.toString() == "" || inpResult.text.toString() == null) {
-                tvResultado.text = "Por favor, insira um valor válido!"
+            // Checamos se o etNome está em branco
+            if (etNome.text.isBlank()) {
+                // Exibir mensagem de erro
+                etNome.error = "Digite um nome válido!"
             } else {
-                tvResultado.text = inpResult.text.toString()
+                // Atualização de texto do TextView
+                tvResultado.text = etNome.text
             }
         }
+
+        // Abrir a Activity Resultado
+        // Registra a intenção (Intent) de abrir a tela no Android
+
+        val btAbrirNovaTela = findViewById<Button>(R.id.btAbrirNovaTela)
+
+        btAbrirNovaTela.setOnClickListener {
+            // Para abrir a Activity: Intent que sabe onde estamos e para onde vamos
+            //   -> De onde estamos: this (MainActivity)
+            //   -> Para onde vamos: ResultadoActivity::class.java
+            val resultadoIntent = Intent(this, ResultadoActivity::class.java)
+
+            // Adicionar dados à Intent (putExtra)
+            // -> Nome para a informação: NOME_DIGITADO
+            // -> Valor da informação: String obtido a partir de etNome.text
+            resultadoIntent.putExtra("NOME_DIGITADO", etNome.text.toString())
+
+            // Inicia a tela a partir dessa Intent
+            startActivity(resultadoIntent)
+        }
+
+        // Próximos passos:
+        // 1. Aprender mais sobre as arquiteturas: MVM e MVVM
+        // 2. Aprender sobre o Ciclo de Vida de Activity e da Aplicação
+        // 3. Desenvolver um mini projeto com apelo visual que envolva o consumo de uma WebService
+        // Ler o Livro: https://github.com/FabricaDeSinapse/livro-android-casa-do-codigo/
     }
 }
